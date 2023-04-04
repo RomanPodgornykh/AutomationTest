cf83d61f7c47fbf8232a8eec76440053842fb3ce
Tester Tester <romanpodgornykh@gmail.com>
2023-04-04 20:18:47 +0200

    040423 20:18

--- src/main/java/AutoTest.java
+++ src/main/java/AutoTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -9,9 +11,17 @@ public class AutoTest {
         co.addArguments("--remote-allow-origins=*");
         System.setProperty("webdriver.chrome.driver","C:\\IdeaProjects\\AutomationTest\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(co);
+        driver.manage().window().fullscreen();
         driver.get("https://demoqa.com/");
-        driver.wait(5000);
-        driver.findElement(By.cssSelector("//h5[contains(text(),'Book Store Application')]"));
+
+        //driver.wait(5000);
+
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("scrollBy(0,2500)");
+        //driver.findElement(By.cssSelector("//h5[contains(text(),'Book Store Application')]")).click();
+        driver.findElement(By.cssSelector("div.body-height:nth-child(2) div.home-content div.home-body div.category-cards div.card.mt-4.top-card:nth-child(6) div:nth-child(1) div.card-body > h5:nth-child(1)")).click();
+
+
     }
 }
 
