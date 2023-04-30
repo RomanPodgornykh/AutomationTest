699d97b03dfdc0c3d1ed0e7e2e400ce6189835a0
Roman Podhornykh <romanpodgornykh@gmail.com>
2023-05-01 01:26:27 +0300

    240423

--- src/main/java/LoginTestCase.java
+++ src/main/java/LoginTestCase.java
@@ -4,25 +4,32 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginTestCase {
     public static void main(String[] args) throws InterruptedException {
         ChromeOptions co = new ChromeOptions();
         co.addArguments("--remote-allow-origins=*");
-        System.setProperty("webdriver.chrome.driver","C:\\IdeaProjects\\AutomationTest\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\IdeaProjects\\AutomationTest\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(co);
-        LoginPageFactory LoginPage = new LoginPageFactory(driver);
+        //LoginPageFactory LoginPage = new LoginPageFactory(driver);
         driver.get("https://demoqa.com/");
         //driver.wait(3000);
         driver.manage().window().maximize();
-        JavascriptExecutor js = (JavascriptExecutor)driver;
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("scrollBy(0,2500)");
 
         driver.findElement(By.xpath("//h5[contains(text(),'Book Store Application')]")).click();
-        driver.findElement(By.xpath("//button[@id='login']")).click();
-        LoginPage.login("JohnTron", "Demoqa1!");
-        //js.executeScript("scrollBy(0,2500)");
         Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[@id='login']")).click();
 
-    }
-}
+        LoginPageFactory loginPage = PageFactory.initElements(driver, LoginPageFactory.class);
+        loginPage.login("JohnTron", "Demoqa1!");
+        boolean isSuccessful = loginPage.isLoginSuccessfull();
+        if(isSuccessful) {
+            System.out.println("Login was successful!");}
+        else {
+            System.out.println("Login failed");
+        };
+        driver.close();
+    }}
--- src/main/java/PageObjects/LoginPageFactory.java
+++ src/main/java/PageObjects/LoginPageFactory.java
@@ -1,27 +1,45 @@
 package PageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
 public class LoginPageFactory {
-    private WebDriver driver;
+    public WebDriver driver;
     @FindBy(css = "#userName")
-private WebElement usernameField;
+    private WebElement usernameField;
     @FindBy(css = "#password")
-private WebElement passwordField;
+    private WebElement passwordField;
+    @FindBy(xpath = "//button[@id='login']")
+    private WebElement loginbutton;
+    @FindBy(xpath = "#userName-label")
+    private WebElement usernamelabel;
 
-    public LoginPageFactory(WebDriver driver){
-    this.driver = driver;
-        PageFactory.initElements(driver, this);
-
-    }
-
-    public void login(String username, String password){
+    public void login(String username, String password) {
         usernameField.sendKeys(username);
         passwordField.sendKeys(password);
-        passwordField.submit();
+        loginbutton.click();
+
 
     }
+
+    public boolean isLoginSuccessfull() {
+        try {
+            return usernamelabel.isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+   }
 }
+
